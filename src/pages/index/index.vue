964c02f9079b007cdf60c070dee63320e44b6363
sunjunwei <sunjunwei@youbeile.com.cn>
2020-10-28 17:15:19 +0800

    第二次提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="index">
+      <div class="search">
+      <div></div>
+      <div>
+        <input type="text" placeholder="搜索商品">
+        <span class="icon"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import "./style.scss";
+</style>
